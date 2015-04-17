d91228f0f2c9566163a21c85022c284fbb04529c
Christophe Rosset <tophe@topheman.com>
2015-04-17 19:51:49 +0200

    better error display management

--- server/routes.js
+++ server/routes.js
@@ -3,12 +3,17 @@ var router = express.Router();
 var path = require('path');
 
 /* GET users listing. */
-router.get('/*', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render(path.resolve(__dirname, 'views/body.ejs'), {
     layout: 'layout',
     body: '<p>Hello world!</p>',
     env: req.app.get('env')
   });
+});
+
+/* if no route matched, redirect home */
+router.get('/*', function(req, res, next) {
+  res.redirect('/');
 });
 
 module.exports = router;
