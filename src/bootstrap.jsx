bae7d84aab1a803d351853a7e1fefe078b305ff3
Christophe Rosset <tophe@topheman.com>
2015-04-17 19:57:54 +0200

    [client]Added Router.HistoryLocation

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -9,7 +9,7 @@ httpService.getInstance(httpServiceConfiguration);//will keep config in singleto
 //this way, instead of using resolve.alias of webpack (and having the require of module messed up by webpack when they'll be executed server-side)
 //I use dependency injection, in the one place that won't be executed in node : the client side bootstrap
 
-Router.run(routes, function(Handler, state){
+Router.run(routes, Router.HistoryLocation, function(Handler, state){
   var params = state.params;//pass params to handler (this way we will pass data on the express router) https://github.com/rackt/react-router/blob/master/docs/guides/overview.md#dynamic-segments
   React.render(<Handler params={params}/>, document.body);
 });
