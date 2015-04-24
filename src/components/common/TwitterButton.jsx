463df915c69cb6115894a3eef1053932d23b4051
Christophe Rosset <tophe@topheman.com>
2015-04-24 20:19:08 +0200

    [client] changed twitter button message

--- src/components/common/TwitterButton.jsx
+++ src/components/common/TwitterButton.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default class TwitterButton extends React.Component {
   render(){
     return (
-      <a href="https://twitter.com/share" className="twitter-share-button" data-text="Simple #reactjs #es6 using Github API" data-via="topheman" data-size="large" data-count="none" data-url="https://topheman.github.io/react-es6/">Tweet</a>
+      <a href="https://twitter.com/share" className="twitter-share-button" data-text="Isomorphic #reactjs app in #es6 using Github API" data-via="topheman" data-size="large" data-count="none" data-url="https://topheman-react-es6-isomorphic.herokuapp.com/">Tweet</a>
     );
   }
 }
