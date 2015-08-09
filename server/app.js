728eaf8652d6ee9c984d5fa5930794afb04d64f7
Christophe Rosset <tophe@topheman.com>
2015-08-09 02:11:36 +0200

    changed favicon

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.set('layout', 'layout');
 app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname ,'../public/favicon.png')));
+app.use(favicon(path.join(__dirname ,'../public/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
