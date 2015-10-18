a48d5ebb70841603deff631d1c005fe53f4c4dae
Christophe Rosset <tophe@topheman.com>
2015-10-18 21:14:23 +0200

    feat(*) message it's a rc version

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,6 +13,7 @@ export default class Home extends React.Component {
         <p><strong>TL;DR</strong> : click on the button to try it !</p>
         <p className="text-center"><Link className="btn btn-default btn-primary btn-lg" to="/github">TRY the DAMN thing !</Link></p>
         <p><strong>Isomorphic</strong>, you say ? You may now use the term <i>"universal"</i> ... Take a quick look at the <strong><Link to="/about">about page</Link></strong> if you don't see the difference with the front-only version.</p>
+        <p className="lead"><strong>NB:</strong> You are looking at a <strong>release candidate version</strong>, not yet integrated on master. The code is on that <a href="https://github.com/topheman/react-es6-isomorphic/tree/feature/update-react-build-routine" title="Browse the new feature">branch</a>.</p>
       </div>
     );
   }
